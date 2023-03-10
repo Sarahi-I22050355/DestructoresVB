3f2b8ae55e270ac58ac240452cbb20d1854179f1
Sarah <Sarah@HP-SARAHI>
2023-03-09 22:49:51 -0600

    Agregar archivos de proyecto.

---
+++ ManejoArchivos.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Public Class ManejoArchivos
+    Private archivo As StreamReader = Nothing
+    Private contador As Integer = 0
+    Private linea As String
+
+    Public Sub New()
+        archivo = New StreamReader("C:\Users\Sarah\OneDrive\Escritorio\data.txt")
+        While (InlineAssignHelper(linea, archivo.ReadLine())) IsNot Nothing
+            Console.WriteLine(linea)
+            contador += 1
+        End While
+    End Sub
+
+    Public Sub Mensaje()
+        Console.WriteLine("Hay {0} lineas", contador)
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        archivo.Close()
+    End Sub
+
+    Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, value As T) As T
+        target = value
+        Return value
+    End Function
+End Class
---
+++ Module1.vb
@@ -0,0 +1,7 @@
+﻿Module Module1
+    Sub Main(args As String())
+        Dim miarchivo As New ManejoArchivos()
+        miarchivo.Mensaje()
+        Console.ReadKey()
+    End Sub
+End Module
